d5a546dc8e2fc7b102a3ec4dcc9e1bc207240d38
DONADEI Emrick <emrick.donadei@hotmail.fr>
2020-10-17 17:30:33 +0200

    first migration working

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
   const post = orm.em.create(Post, { title: "my first post" });
   await orm.em.persistAndFlush(post);
-  await orm.em.nativeInsert(Post, { title: "My first post" });
 };
 
 main().catch(err => {
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,15 +2,17 @@ import { fromCallback } from "bluebird";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
-import  path  from "path";
+import path from "path";
 
 export default {
-    entities: [Post],
-    dbName: "lireddit",
-    user: "postgres",
-    password: "qwerty",
-    type: "postgresql",
-    path: path.join(__dirname, './migrations'), 
-    pattern: /^[\w-]+\d+\.[tj]s$/, 
-    debug: !__prod__
-    } as Parameters<typeof MikroORM.init>[0];
+  entities: [Post],
+  dbName: "lireddit",
+  user: "postgres",
+  password: "qwerty",
+  type: "postgresql",
+  debug: !__prod__,
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/
+  }
+} as Parameters<typeof MikroORM.init>[0];
