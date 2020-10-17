e4a6d63f27b64eb7a0d75f2317e6511fa4cfd717
DONADEI Emrick <emrick.donadei@hotmail.fr>
2020-10-17 17:04:38 +0200

    ignoring dist folder adding mikro orm config

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,15 @@
+import { fromCallback } from "bluebird";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import  path  from "path";
+
+export default {
+    entities: [Post],
+    dbName: "lireddit",
+    user: "postgres",
+    password: "qwerty",
+    path: path.join(__dirname, './migrations'), 
+    pattern: /^[\w-]+\d+\.[tj]s$/, 
+    debug: !__prod__
+    } as Parameters<typeof MikroORM.init>[0];
